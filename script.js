13d7a4940fd831c39ec37393ef47aca775896567
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-06-29 15:03:28 -0500

    added current day'

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+let currentDayBox = $('#currentDay');
+let currentDay = dayjs();
+currentDayBox.text(currentDay.format("dddd, MMM DD"));
+// .text("hi!");
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
