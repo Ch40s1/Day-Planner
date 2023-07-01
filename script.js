9b81be9d82be3226590c75822c238ec055a2f2dd
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-07-01 13:05:49 -0500

    added save to local storage

--- script.js
+++ script.js
@@ -14,7 +14,53 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  /* Think of every problem as input and outputs
+    make pseudocode for problem
+    use docs to fill in pseudocode
+    test every line with console.log 
+    if messed up look up solutions
+    repeat */
+  //select text box
+  const description = $('.description');
+  console.log(description[0]);
+  //select save button
+  const saveBtns = $('.saveBtn');
+
+  saveBtns.each(function() {
+  //save button evetn click 
+  $(this).on("click", () => {
+    //target is the number associated with the description 
+    const target = $(this).data('target');
+    //this combines the class description with the number (target) 
+    const description = $('.description[data-target="' + target + '"]');
+    console.log(description);
+    //value is what is written in text box
+    const value = description.val();
+    console.log(value);
+    value !== '' ? localStorage.setItem(target, value) : console.log('empty');
+  });
+});
+/*get description from local storage 
+description x. text = localstorage get item that matches x.
+find corresponding class based on local storage name
+write text to text area. */
+
+// console.log(localStorage.getItem('description1'));
+
+
+
+  // const saveBtns = $('.saveBtn');
+  // saveBtns.each(function() {
+  //   $(this).on("click", () => {
+  //     const value = description[0].value;
+  //     value !== '' ? localStorage.setItem("Description", value) : console.log('empty');
+  //   });
+  // });
+// if(description[0].value !==  ''){
+    //   localStorage.setItem("Description", description.val());
+    // } else {
+    //   console.log('empty');
+    // }
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
