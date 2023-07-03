a0146c6fa92ca3b43220b1bb46ebd47a7a038e1b
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-07-03 15:29:59 -0500

    made a div container clone using jquery for future iteration

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 // in the html.
 let currentDayBox = $('#currentDay');
 let currentDay = dayjs();
-let currentHour = dayjs().format("H");
-currentDayBox.text(currentDay.format("dddd, MMM DD " + currentHour + ":mm a"));
+let currentHour = dayjs().format("h");
+let currentMeridiem = dayjs().format('a');
+currentDayBox.text(currentDay.format("dddd, MMM DD h:mm a"));
+console.log(currentMeridiem);
 // .text("hi!");
-
+let divCounter = 0;
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -21,6 +23,63 @@ $(function () {
     test every line with console.log 
     if messed up look up solutions
     repeat */
+    // <div id="hour-9" class="row time-block">
+    //     <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+    //     <textarea class="col-8 col-md-10 description" rows="3" "> </textarea>
+    //     <button class="btn saveBtn col-2 col-md-1" aria-label="save"">
+    //       <i class="fas fa-save" aria-hidden="true"></i>
+    //     </button>
+    // </div>
+    // const divContainer = $(".container-fluid");
+    // const rowDiv = createElement('div');
+    // rowDiv.setAttribute("id","hour-" + currentHour);
+    // rowDiv.setAttribute('class', 'row time-block');
+    // const stylingDiv = createElement('div');
+    // stylingDiv.setAttribute('class', 'col-2 col-md-1 hour text-center py-3');
+    // stylingDiv.textContent = `${currentHour}${currentMeridiem}`;
+    // const textArea = createElement('textarea');
+    // textArea.setAttribute('col-8 col-md-10 description');
+    // textArea.setAttribute("rows", '3');
+    // const btn = createElement('button');
+    // btn.setAttribute('class', 'btn saveBtn col-2 col-md-1');
+    // btn.setAttribute('aria-label', 'save');
+    // const saveIcon = createElement('i');
+    // saveIcon.setAttribute('class', 'fas fa-save');
+    // saveIcon.setAttribute('aria-hidden', 'true');
+
+
+    // rowDiv.appendChild(stylingDiv);
+    // rowDiv.appendChild(textArea);
+    // rowDiv.appendChild(btn);
+    // btn.appendChild(saveIcon);
+
+    // divContainer.appendChild(rowDiv);
+const divContainer = $(".container-fluid");
+
+const rowDiv = $("<div>").attr("id", "hour-" + currentHour).addClass("row time-block");
+
+const stylingDiv = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(currentHour + currentMeridiem);
+
+const textArea = $("<textarea>").addClass("col-8 col-md-10 description").attr("rows", "3");
+
+const btn = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
+
+const saveIcon = $("<i>").addClass("fas fa-save").attr("aria-hidden", "true");
+
+btn.append(saveIcon);
+rowDiv.append(stylingDiv, textArea, btn);
+divContainer.append(rowDiv);
+
+
+  //loop to create divs based on hours a day
+  // for(var i =0; i<12; i++){
+
+  // }
+
+  //add class to first div
+  //append div to class container-fluid
+
+
 
   // Select text box
 const userText = $('.description');
@@ -61,13 +120,28 @@ let timeSection = $('.time-block');
   //
 timeSection.each(function(){
   //set testing current hour
+  let currentHour = 11;
   //get current id of timeSection
+  const idOfTimeSection = $(this).attr('id');
+  //uses match method to extract the number part of the id and converts it to a integer
+  const numericId = parseInt(idOfTimeSection.match(/\d+/)[0]);
    //if id < current hour
+   if(numericId < currentHour){
    //then set class to past 
+    // $(this).removeClass("past" || "present" || "future");
+    $(this).addClass('past')
+   }else if(numericId > currentHour){
+    // $(this).removeClass("past" || "present" || "future");
+    $(this).addClass('future')
+   }else{
+    // $(this).removeClass("past" || "present" || "future");
+    $(this).addClass('present')
+   }
    //else if id > current hour 
    //then set class to future
    //else
    //set class to current
+
 });
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
